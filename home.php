71d11160b7f4af181253b9b9e8027ddb23fb84d5
Kidd-nash <arkady.pantaleon@outlook.com>
2024-08-29 20:45:21 +0800

    added a from for insert/create

--- home.php
+++ home.php
@@ -6,12 +6,22 @@ $arr = [];
 $result = pg_query($conn, "SELECT * FROM new_cars ORDER BY id");
 $arr = pg_fetch_all($result);
 
-?>
+?> 
 
 <!DOCTYPE html>
 <html lang="en">
     <head></head>
     <body>
+    <form method="POST" action="/create.php">
+            <h2>Input Part</h2>
+            <label>Brand:</label>
+            <input type="text" name="brand" placeholder="Insert Brand..."/><br>
+            <label>Model:</label>
+            <input type="text" name="model" placeholder="Insert Model..."/><br>
+            <label>Year:</label>
+            <input type="number" name="year" placeholder="Year..." min="1886" max="2025" /><br>
+            <button type="submit">Submit</button>
+        </form>
         <table>
             <thead>
             <td>Brand</td>
@@ -33,5 +43,6 @@ $arr = pg_fetch_all($result);
                 }
             ?>
         </table>
+
     </body>
 </html>

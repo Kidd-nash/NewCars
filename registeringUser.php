6342572a18fa9e37be085750f9a9c6f54de166b9
Kidd-nash <arkady.pantaleon@outlook.com>
2024-08-30 15:33:09 +0800

    added a file for registering users

---
+++ registeringUser.php
@@ -0,0 +1,19 @@
+<?php 
+     ob_start();
+     if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        // include_once("./connection.php");
+ 
+        $user_name = $_POST["userName"];
+        $password = $_POST["password"];
+         
+        echo "Your username is: " . $user_name;
+        echo "Your password is: " . $password;
+         // TODO: add validation
+ 
+        //  $result = pg_query($conn, "INSERT INTO trial_users (userName, password) VALUES ('$user_name', '$password');");
+ 
+     }
+     ob_end_clean();
+    //  header("Location: http://localhost:8080/newCars/home.php");
+    //  die();
+

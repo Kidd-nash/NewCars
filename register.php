5315828ccb034a98008dee58cb95b8b29ad66c14
Kidd-nash <arkady.pantaleon@outlook.com>
2024-08-30 15:32:35 +0800

    added a file for register

---
+++ register.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Register</title>
+    </head>
+    <body>
+        <form method="POST" action="/newCars/registeringUser.php">
+            <h2>Register</h2>
+            <label>Username:</label>
+            <input type="text" name="userName" placeholder="Create Username..."/><br>
+            <label>Password:</label>
+            <input type="text" name="password" placeholder="Create Password..."/><br>
+            <button type="submit">Submit</button>
+        </form>
+    </body>
+</html>

e16343ed2f0799957285b89863252c5e973c249f
Kidd-nash <arkady.pantaleon@outlook.com>
2024-09-03 19:03:41 +0800

    created an initial home page for log in redirect

---
+++ newTrial/homePage.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            Home Page
+        </title>
+    </head>
+    <body>
+        <h1>
+            This is the home page...
+        </h1>
+        <p>
+            !NOTE to edit
+        </p>
+    </body>
+</html>

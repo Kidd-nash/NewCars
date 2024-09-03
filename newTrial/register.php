25c46513cb500c0f4911f17376a5784aa0d698f1
Kidd-nash <arkady.pantaleon@outlook.com>
2024-09-03 18:54:13 +0800

    created a register form page, and linked initial page to it

---
+++ newTrial/register.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Register</title>
+    </head>
+    <body>
+        <form>
+            <h2>Register</h2>
+            <label>E-mail:</label>
+            <input type="text" name="email" placeholder="Insert Email..."/><br>
+            <label>Username:</label>
+            <input type="text" name="userName" placeholder="Create Username..."/><br>
+            <label>Password:</label>
+            <input type="text" name="password" placeholder="Create Password..."/><br>
+            <button type="submit">Submit</button>
+        </form>
+    </body>
+</html>
+
+<!-- form attribute:  method="POST" action="/newCars/registeringUser.php" -->

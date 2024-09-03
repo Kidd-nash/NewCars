cc76f1e8251d8243b0ede28b4269c4bda5bc0004
Kidd-nash <arkady.pantaleon@outlook.com>
2024-09-03 19:56:51 +0800

    created a log in pg query does not work and does not show erros

--- newTrial/loggingInUser.php
+++ newTrial/loggingInUser.php
@@ -9,39 +9,18 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
    $password = $_POST["password"];
    $hashPassword = md5($password);
 
-//    $result = pg_query($conn, "SELECT * FROM trial_users WHERE username = '$user_name' AND password = '$hashPassword';");
-//    $user = pg_fetch_assoc($result);
+   $result = pg_query($conn, "SELECT * FROM new_trial_users WHERE username = '$user_name_or_email' OR email = '$user_name_or_email' AND password = '$hashPassword';");
+   $user = pg_fetch_assoc($result);
 
-//    $userId = $user['id'] ?? null;
+   $userId = $user['id'] ?? null;
 
-//    print_r($user);
-//    if (!is_null($userId)) {
-//         $_SESSION['userId'] = $userId;
-//         $_SESSION['userName'] = $user['username'];
-//         ob_end_clean();
-//         header("Location: http://localhost:8080/newCars/home.php");
-//         die();
-//    }
+   print_r($user);
+   if (!is_null($userId)) {
+        $_SESSION['userId'] = $userId;
+        $_SESSION['userName'] = $user['username'];
+        ob_end_clean();
+        header("Location: http://localhost:8080/newCars/newTrial/homePage.php");
+        die();
+   }
    
 }
-?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>
-            Loggin In User
-        </title>
-    </head>
-    <body>
-        <p>
-            <?php
-                echo "The username / email you entered: " . $user_name_or_email;
-            ?>
-        </p>
-        <p>
-            <?php
-                echo "The password you entered: " . $password;
-            ?>
-        </p>
-    </body>
-</html>

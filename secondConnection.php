2956baf1a99567ca6cb3e5396ae675d41312b2c7
Kidd-nash <arkady.pantaleon@outlook.com>
2024-09-04 14:45:03 +0800

    created a second connection with PDO

---
+++ secondConnection.php
@@ -0,0 +1,23 @@
+<?php 
+
+
+$hostname = 'db_postgres_lab'; //get host name
+
+$dbname = 'first'; // Set the database name
+
+$username = 'pguser'; // Set the username with permissions to the database
+
+$password = 'pgpwd'; // Set the password with permissions to the database
+
+$dsn = "pgsql:host=$hostname;dbname=$dbname"; // Create the DSN (data source name) by combining the database type (PostgreSQL), hostname and dbname
+
+$db = new PDO($dsn, $username, $password); //Create PDO
+
+// try {
+//     $db = new PDO($dsn, $username, $password);
+//  } catch (Exception $e) {
+//    echo $e->getMessage();
+//  }
+
+//  var_dump($db);
+//  die();

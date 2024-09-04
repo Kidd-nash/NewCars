d3aff3a95310d56dfefca8d39294170b57cf88e6
Kidd-nash <arkady.pantaleon@outlook.com>
2024-09-04 15:04:07 +0800

    refactoed create to pdo

--- create.php
+++ create.php
@@ -1,7 +1,7 @@
 <?php
     ob_start();
     if($_SERVER['REQUEST_METHOD'] == 'POST') {
-        include_once("./connection.php");
+        include_once("./secondConnection.php");
 
         $brand = $_POST["brand"];
         $model = $_POST["model"];
@@ -9,7 +9,11 @@
 
         // TODO: add validation
 
-        $result = pg_query($conn, "INSERT INTO new_cars (brand, model, year) VALUES ('$brand', '$model', '$year');");
+        // $result = pg_query($conn, "INSERT INTO new_cars (brand, model, year) VALUES ('$brand', '$model', '$year');");
+
+        $userQueary = $db->prepare("INSERT INTO new_cars (brand, model, year) VALUES (:brand, :model, :year)");
+
+        $userQueary->execute(['brand' => $brand, 'model' => $model, 'year' => $year]);
 
     }
     ob_end_clean();

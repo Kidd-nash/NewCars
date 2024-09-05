6aa51a5a6909b7946e41f77336f832a60472c10f
Kidd-nash <arkady.pantaleon@outlook.com>
2024-09-05 11:59:52 +0800

    fixed query fetch

--- edit.php
+++ edit.php
@@ -6,13 +6,13 @@ include_once("./secondConnection.php");
 
     // $result = pg_query($conn, "SELECT * FROM new_cars WHERE id = $editId");
 
-    $userQuery = $db->prepare('SELECT * FROM new_cars WHERE id = :editId');
+    $carQuery = $db->prepare('SELECT * FROM new_cars WHERE id = :editId');
     // Execute the statement here
-    $userQuery->execute(['editId' => $editId]);
+    $carQuery->execute(['editId' => $editId]);
 
     // $newCar = pg_fetch_assoc($result);
 
-    $carQuery = $db->query('SELECT * FROM new_cars');
+    // $carQuery = $db->query('SELECT * FROM new_cars');
     // Fetch one book using the fetch() method and assign it to the $book variable.
     $car = $carQuery->fetch(PDO::FETCH_ASSOC);
 
@@ -30,7 +30,7 @@ include_once("./secondConnection.php");
         <?php if (isset($_SESSION['userName'])): ?>
             <p>Hi, <?php echo $_SESSION['userName'] ?>
         <?php endif; ?>
-        <form method="POST" action="/carUpdate.php">
+        <form method="POST" action="/newCars/carUpdate.php">
             <label>Brand:</label>
             <input type="text" name="brand" value="<?php echo $brand ?>"/>
             <label>Model:</label>

d6d06070e718554758aeda704869c0861b646c37
Kidd-nash <arkady.pantaleon@outlook.com>
2024-09-05 10:43:13 +0800

    fixed delete method/function switch to prepare and execute PDO

--- carDelete.php
+++ carDelete.php
@@ -1,13 +1,17 @@
 <?php 
     ob_start();
 
-    include_once("./connection.php");
+    include_once("./secondConnection.php");
 
     $deleteId = $_GET["deleteId"] ?? null;
 
     echo "Deleting Car data id:" . $deleteId;
     
-    $result = pg_query($conn, "DELETE FROM new_cars WHERE id = $deleteId");
+    // $result = pg_query($conn, "DELETE FROM new_cars WHERE id = $deleteId");
+
+    $userQuery = $db->prepare("DELETE FROM new_cars WHERE id = :deleteId");
+    
+    $userQuery->execute(['deleteId' => $deleteId]);
 
     ob_end_clean();
 

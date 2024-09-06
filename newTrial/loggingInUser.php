757560c8ac85e4068b6fa7bcb65df034bd39ca87
Kidd-nash <arkady.pantaleon@outlook.com>
2024-09-06 12:52:05 +0800

    changed method to pdo and fixed syntax

--- newTrial/loggingInUser.php
+++ newTrial/loggingInUser.php
@@ -8,8 +8,15 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
    $password = $_POST["password"];
    $hashPassword = md5($password);
 
-   $result = pg_query($conn, "SELECT * FROM new_trial_users WHERE (username = '$user_name_or_email' OR email = '$user_name_or_email') AND password = '$hashPassword';");
-   $user = pg_fetch_assoc($result);
+   // $result = pg_query($conn, "SELECT * FROM new_trial_users WHERE (username = '$user_name_or_email' OR email = '$user_name_or_email') AND password = '$hashPassword';");
+   // $user = pg_fetch_assoc($result);
+
+   // Write an equivalent prepared statement here
+   $userQuery = $db->prepare('SELECT * FROM new_trial_users WHERE (username = :user_name_or_email OR email = :user_name_or_email) AND password = :hashpassword');
+   // Execute the statement here
+   $userQuery->execute(['user_name_or_email' => $user_name_or_email, 'hashpassword' => $hashPassword]);
+   // Fetch user
+   $user = $userQuery->fetch(PDO::FETCH_ASSOC);
 
    $userId = $user['id'] ?? null;
 

93901b276619bbf216dc8af7b20f59c5a9809d25
Kidd-nash <arkady.pantaleon@outlook.com>
2024-09-06 12:51:36 +0800

    fixed names

--- newTrial/homePage.php
+++ newTrial/homePage.php
@@ -27,9 +27,9 @@ $trials = $trialQuery->fetchAll(PDO::FETCH_ASSOC);
         </h1>
         <form method="POST" action="/newCars/newTrial/create.php">
             <h2>Input Part</h2>
-            <label>Brand:</label>
+            <label>Character:</label>
             <input type="text" name="character" placeholder="Insert Name..."/><br>
-            <label>Model:</label>
+            <label>Series:</label>
             <input type="text" name="series" placeholder="Insert Series..."/><br>
             <button type="submit">Submit</button>
         </form>
@@ -46,7 +46,7 @@ $trials = $trialQuery->fetchAll(PDO::FETCH_ASSOC);
                         echo "<td>" . $item["character"] . "</td>";
                         echo "<td>" . $item["series"] .  "</td>";
                         echo "<td><a href='edit.php?editId=" . $item["id"] . "'>Edit</a></td>";
-                        echo "<td><a href='carDelete.php?deleteId=" . $item["id"] . "'>Delete</a></td>";
+                        echo "<td><a href='delete.php?deleteId=" . $item["id"] . "'>Delete</a></td>";
                     echo "</tr>";
                   
                 }
